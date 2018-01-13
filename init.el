c5dc107197b965c3e3b439c64d2cdad2e714a9da
Khanh Tran <nkhanh.trn@gmail.com>
2018-01-12 20:00:48 -0500

    change package manager for good

--- init.el
+++ init.el
@@ -4,12 +4,15 @@
 
 ; package manager
 (require 'package)
-(dolist (source '(("gnu" . "https://elpa.gnu.org/packages/")
-                  ("marmalade" . "https://marmalade-repo.org/packages/")
-                  ("elpa" . "http://tromey.com/elpa/")
-                  ("melpa" . "https://melpa.org/packages/")
-                  ))
-  (add-to-list 'package-archives source t))
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
 ; autoload packages at start
@@ -78,3 +81,19 @@
 (load-library "nk-webmode")                    ; web-mode
 (load-library "nk-javascript")                 ; JavaScript mode
 (load-library "nk-typescript")                 ; TypeScript mode
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (solarized-theme golden-ratio rainbow-mode smooth-scrolling nyan-mode dired-rainbow dired+ dired-details+ markdown-mode lua-mode function-args yasnippet helm-descbinds helm-projectile helm-gtags helm tide tern-auto-complete tern js-comint js2-mode web-mode elisp-slime-nav ws-butler dtrt-indent clean-aindent-mode smartparens sr-speedbar company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
